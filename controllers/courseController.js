2c5586ed0aac6788041642ba8d445e33eb8c7b94
imdurgadas <imdurgadas@gmail.com>
2020-12-19 14:24:12 +0530

    populate nested field

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -12,7 +12,14 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
     if (req.params.bootcampId) {
         query = Course.find({ bootcamp: req.params.bootcampId });
     } else {
-        query = Course.find();
+        //populaate entire bootcamp within course
+        //query = Course.find().populate('bootcamp');
+
+        //get courses, populate bootcamp but only name description
+        query = Course.find().populate({
+            path: 'bootcamp',
+            select: 'name description'
+        });
     }
 
     const courses = await query;
