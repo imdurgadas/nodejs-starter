e54dc632e91f40f60306bad323e6548674c904a1
imdurgadas <imdurgadas@gmail.com>
2020-12-20 11:29:03 +0530

    Additional method

--- controllers/authController.js
+++ controllers/authController.js
@@ -45,8 +45,21 @@ exports.login = asyncHandler(async (req, res, next) => {
     sendTokenResponse(user, 200, res);
 });
 
-//Get token from model , create cookie and send response
 
+// @desc Get logged in user
+// @route GET /api/v1/auth/me
+// @access Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+    const user = req.user;
+    return res.status(200).json({
+        success: true,
+        data: user
+    })
+});
+
+
+
+//Get token from model , create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
 
     //Create token
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const Router = express.Router();
-const { register, login } = require('../controllers/authController');
+const { register, login, getMe } = require('../controllers/authController');
+const { protect } = require('../middlewares/auth');
 const router = require('./courseRoutes');
 
 router.route('/register').post(register);
 router.route('/login').post(login);
-
+router.route('/me').get(protect, getMe);
 module.exports = router;
 
